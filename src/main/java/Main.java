b8eca7b0276a7c939ea07b024c8361eda0ac5348
WIMSik <bartek.zagula@gmail.com>
2020-04-05 09:18:17 +0200

    zadanje

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,22 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("to ja");
+        Person person1 = new Person("Bartek ");
+        Person person2 = new Person("Anna");
+        Person person3 = new Person("Jola");
+
+        person1.addMoney(666,Currency.EUR);
+        person1.addMoney(2000);
+        person2.addMoney(200,Currency.USD);
+        person3.addMoney(9000);
+
+        System.out.println(person1.toString());
+        System.out.println(person2.toString());
+        System.out.println(person3.toString());
+        System.out.println("==============================");
+        person1.removeMoney(100,Currency.EUR);
+        person2.addMoney(100,Currency.EUR);
+        System.out.println(person1.toString());
+        System.out.println(person2.toString());
+        System.out.println(person3.toString());
     }
 }
